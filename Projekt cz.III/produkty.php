9e19d60e560bdb8580cbb6dd50d36b2e6c79d927
tomasz78285 <62183978+tomasz78285@users.noreply.github.com>
2020-06-09 21:59:15 +0200

    Update produkty.php

--- Projekt cz.III/produkty.php
+++ Projekt cz.III/produkty.php
@@ -26,13 +26,133 @@
     echo "<p>Witaj ".$_SESSION['user'].'! [ <a href="wyloguj.php">Wyloguj się!</a> ]</p>';
     ?>
  
-        
+                     <form action="produkt.php" method="POST">
+                       <input type="submit" value="Spis produktów">
+                       </form>
+           
             <form action="dodajNowyProdukt.php" method="POST">    
                 <article class="kk">
                         <section class="s1">
                             Nazwa
                             <br><br>
                             <input type="text" name="Nazwa"><br><br>
+                            Nazwa pliku
+                            <br><br>
+                            <input type="text" name="plik"><br><br>
+                            Ilość
+                            <br><br>
+                            <input type="number" name="Ilosc"><br><br>
+                            Cena
+                            <br><br>
+                            <input type="text" name="Cena"><br><br>
+                    
+
+                            <p>Sekcja</p>
+                             <select name="Sekcja">
+                                <option selected>Łyżwy</option>
+                                <option>Narty</option>
+                                <option>Hokej</option>
+                                <option>Rękawice</option>
+                                <option>Ochraniacze</option>
+                                <option>Koszulki</option>
+                                <option>Koszulki treningowe</option>
+                                <option>Piłki</option>
+                                <option>Buty(Sporty letnie)</option>
+                                <option>Pływanie</option>
+                                <option>Plecaki</option>
+                                <option>Białko</option>
+                                <option>Batony</option>
+                                <option>Napoje</option>
+                                <option>Bluzy</option>
+                                <option>Spodnie</option>
+                                <option>Buty</option>
+                                <option>Czapki</option>
+                            </select>
+                            <br><br>
+                            <section class="s2">
+                               <input type="submit" value="Dodaj produkt">
+                        </section> 
+                        </section>
+            </form>
+
+        <form action="Usun.php" method="GET">             
+                    <article class="kk">
+                        <section class="s1">
+
+
+                            <p>Wybierz produkt</p>
+                            <select name="Produkt">
+                        <?php
+        
+                                  require_once("connect.php");
+                            try
+                            {
+                            $polaczenie = new PDO('mysql:host=' . $mysql_host . ';dbname=' . $database , $username, $password );
+                            }      
+                        
+                        
+                            catch(PDOException $e)
+                            {
+                                echo $e->getMessage();
+                                die();
+                            }
+                            
+                            $sh = $polaczenie -> prepare('SELECT nazwa FROM produkty');
+                            $sh -> execute();
+                            $rows = $sh->fetchAll();
+                            foreach($rows as $row){
+                                echo '<option value="'.$row['id_produktu'].'">'.$row['nazwa'].'</option>';
+                            }
+            
+                     ?>  
+                            </select>
+                            <br><br>
+                        </section>
+                
+               
+                        <section class="s2">
+                               <input type="submit" value="Usun produkt">
+                        </section> 
+                    </article>
+       </form>
+        
+        <form action="Edytuj.php" method="POST">             
+                    <article class="kk">
+                        <section class="s1">
+                            <p>Wybierz produkt</p>
+                            <select name="Produkt">
+                        <?php
+        
+                                  require_once("connect.php");
+                            try
+                            {
+                            $polaczenie = new PDO('mysql:host=' . $mysql_host . ';dbname=' . $database , $username, $password );
+                            }      
+                        
+                        
+                            catch(PDOException $e)
+                            {
+                                echo $e->getMessage();
+                                die();
+                            }
+                            
+                            $sh = $polaczenie -> prepare('SELECT nazwa FROM produkty');
+                            $sh -> execute();
+                            $rows = $sh->fetchAll();
+                            foreach($rows as $row){
+                                echo '<option value="'.$row['id_produktu'].'">'.$row['nazwa'].'</option>';
+                            }
+            
+                     ?>  
+                            </select>
+                            <br><br>
+                      
+                             Nazwa
+                            <br><br>
+                            <input type="text" name="Nazwa"><br><br>
+                            Nazwa pliku
+                            <br><br>
+                            <input type="text" name="plik"><br><br>
                             Ilość
                             <br><br>
                             <input type="number" name="Ilosc"><br><br>
@@ -62,90 +182,11 @@
                                 <option>Buty</option>
                                 <option>Czapki</option>
                             </select>
-                            <br><br>
-                            <section class="s2">
-                               <input type="submit" value="Dodaj produkt">
-                        </section> 
-                        </section>
-            </form>
-
-        <form action="Usun.php" method="POST">             
-            <form action="Usun.php" method="post">
-                    <article class="kk">
-                        <section class="s1">
-
-
-                            <p>Wybierz produkt</p>
-                            <select name="Produkt">
-                                <option selected>Łyżwy hokejowe</option>
-                                <option>Łyżwy figurowe</option>
-                                <option>Łyżwy panczeny</option>
-                                <option>Narty białe</option>
-                                <option>Narty pomarańczowe</option>
-                                <option>Narty czarne</option>
-                                <option>Kask hokejowy</option>
-                                <option>Kij hokejowy</option>
-                                <option>Rękawice do hokeja</option>
-                                <option>Rękawice mma</option>
-                                <option>Rękawice bokserskie</option>
-                                <option>Rękawice kickboxing</option>
-                                <option>Ochraniacze na piszczele</option>
-                                <option>Ochraniacze na głowę</option>
-                                <option>Ochraniacze na zęby</option>
-                                <option>Koszulka czarna</option>
-                                <option>Koszulka biała</option>
-                                <option>Koszulka biała2</option>
-                                <option>Piłka nożna</option>
-                                <option>Piłka rugby</option>
-                                <option>Piłka koszykówka</option>
-                                <option>Buty korki</option>
-                                <option>Buty halowe</option>
-                                <option>Buty do rugby</option>
-                                <option>Okulary</option>
-                                <option>Czepek</option>
-                                <option>Kąpielówki</option></option>
-                                <option>Plecak sportowy duży</option>
-                                <option>Plecak turystyczny</option>
-                                <option>Plecak sportowy mały</option>
-                                <option>Dessert</option>
-                                <option>KFD 80</option>
-                                <option>KFD 90</option>
-                                <option>CFM</option>
-                                <option>Carb Control</option>
-                                <option>Matrix</option>
-                                <option>Master drink</option>
-                                <option>Multipower</option>
-                                <option>Optimum</option>
-                                <option>Bluza1</option>
-                                <option>Bluza2</option>
-                                <option>Bluza3</option>
-                                <option>Spodnie1</option>
-                                <option>Spodnie2</option>
-                                <option>Spodnie3</option>
-                                <option>Buty1</option>
-                                <option>Buty2</option>
-                                <option>Buty3</option>
-                                <option>Czapka1</option>
-                                <option>Czapka2</option>
-                                <option>Czapka3</option>
-                                <option>Bluza4</option>
-                                <option>Bluza5</option>
-                                <option>Bluza6</option>
-                                <option>Spodnie4</option>
-                                <option>Spodnie5</option>
-                                <option>Spodnie6</option>
-                                <option>Buty4</option>
-                                <option>Buty5</option>
-                                <option>Buty6</option>
-                                <option>Czapka4</option>
-                                <option>Czapka5</option>
-                                <option>Czapka6</option>
-                            </select>
-                            <br><br>
-                        </section>
+ 
+                         </section>
                 
                         <section class="s2">
-                               <input type="submit" value="Usuń produkt">
+                               <input type="submit" value="Edytuj produkt">
                         </section> 
                     </article>
        </form>
